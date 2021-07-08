be2892334d07bc3f52894e72a75ed7ccc2f24fd4
Alvaro_Aquije <gonzalo_6824@outlook.com>
2021-07-08 03:37:53 -0500

    initialized repo and started working on project

--- App.tsx
+++ App.tsx
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
 
 export default function App() {
+  const [simpleCounter, setCounter] = useState<number>(0);
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <button
+        onClick={(e) => {
+          setCounter(simpleCounter + 1);
+        }}
+        style={{ backgroundColor: "white", boxShadow: "0 0 10px black" }}
+      >
+        Test buttn
+      </button>
+      <div>{simpleCounter}</div>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  text: {
+    backgroundColor: "#0e0e0e",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    gap: "2rem",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
